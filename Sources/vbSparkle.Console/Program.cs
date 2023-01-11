64e903d6c45a84307fe5b9773f5381a85ba78e0f
Sylvain Bruyere <sbruyere@decompiler-vb.net>
2023-01-11 17:47:53 +0100

    Provide real CLI with syntax Coloring

--- Sources/vbSparkle.Console/Program.cs
+++ Sources/vbSparkle.Console/Program.cs
@@ -1,10 +1,16 @@
 ﻿using CommandLine;
-using System;
+using CommandLine.Text;
+
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using vbSparkle.Options;
+using Colorful;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace vbSparkle.CLI
 {
@@ -12,14 +18,62 @@ namespace vbSparkle.CLI
     {
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-               .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
-               .WithNotParsed(errs => HandleParseError(errs));
+            InitializeConsoleHeader();
+            //1- disable auto generated help
+            var parser = new CommandLine.Parser(with => with.HelpWriter = null);
+
+            //2- run parser and get result
+            var parserResult = parser.ParseArguments<Options>(args);
+
+            parserResult.WithNotParsed(errs => DisplayHelp(parserResult, errs));
+            parserResult.WithParsed(opts => RunOptionsAndReturnExitCode(opts));
 
         }
 
-        private static void HandleParseError(IEnumerable<Error> errs)
+        private static void InitializeConsoleHeader()
         {
+            Console.ForegroundColor = Color.WhiteSmoke;
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.ResetColor();
+            Console.ReplaceAllColorsWithDefaults();
+            Console.Title = "vbSparkle " + version;
+
+            Console.WriteLine(
+                    @"       _     __                  _    _      " + "\r\n" +
+                    @"__   _| |__ / _\_ __   __ _ _ __| | _| | ___ " + "\r\n" +
+                    @"\ \ / / '_ \\ \| '_ \ / _` | '__| |/ / |/ _ \" + "\r\n" +
+                    @" \ V /| |_) |\ \ |_) | (_| | |  |   <| |  __/" + "\r\n" +
+                    @"  \_/ |_.__/\__/ .__/ \__,_|_|  |_|\_\_|\___|" + "\r\n" +
+                    @"               |_|               v" + version
+                  , Color.AliceBlue);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Copyright © Airbus CERT");
+            Console.WriteLine("https://github.com/airbus-cert/vbSparkle");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errors)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = true;
+                h.Heading = System.String.Empty;
+                h.Copyright = System.String.Empty;
+                h.AddEnumValuesToHelpText = true;
+
+                h.AddPreOptionsLine("Sample usage:");
+                h.AddPreOptionsLine("> vbSparkle.CLI -p sample.vbs");
+
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+
+            Console.WriteLine(helpText);
+
+            Console.ReadLine();
         }
 
         private static void RunOptionsAndReturnExitCode(Options opts)
@@ -27,15 +81,15 @@ namespace vbSparkle.CLI
             if (opts.InputFiles.Count() > 0)
                 foreach (var filename in opts.InputFiles)
                 {
-
+                    Console.WriteLine($"# Processing {filename} ...");
                     string fileContent = File.ReadAllText(filename);
 
-                    string result = DeobfuscateContent(fileContent);
+                    string result = DeobfuscateContent(fileContent, opts);
 
                     if (!string.IsNullOrWhiteSpace(opts.Output))
                         File.AppendAllText(opts.Output, result);
                     else
-                        Console.Out.Write(result);
+                        WriteSyntaxColoringConsoleCode(result);
 
                 }
 
@@ -43,16 +97,154 @@ namespace vbSparkle.CLI
             {
                 string fileContent = Console.In.ReadToEnd();
 
-                string result = DeobfuscateContent(fileContent);
+                string result = DeobfuscateContent(fileContent, opts);
 
                 if (!string.IsNullOrWhiteSpace(opts.Output))
                     File.AppendAllText(opts.Output, result);
                 else
-                    Console.Out.Write(result);
+                {
+                    WriteSyntaxColoringConsoleCode(result);
+                   // Console.Out.Write(result);
+                }
             }
         }
 
-        private static string DeobfuscateContent(string content)
+        private static void WriteSyntaxColoringConsoleCode(string result)
+        {
+            Stopwatch perfWatch = new Stopwatch();
+
+            perfWatch.Start();
+
+            string[] vbKeywords = new string[]
+            {
+                "As",
+                "Binary",
+                "ByRef",
+                "ByVal",
+                "Date",
+                "Else",
+                "Empty",
+                "Error",
+                "False",
+                "For",
+                "Friend",
+                "Get",
+                "Input",
+                "Is",
+                "Len",
+                "Let",
+                "Lock",
+                "Me",
+                "Mid",
+                "New",
+                "Next",
+                "Nothing",
+                "Null",
+                "On",
+                "Option",
+                "Optional",
+                "ParamArray",
+                "Print",
+                "Private",
+                "Property",
+                "PtrSafe",
+                "Public",
+                "Resume",
+                "Seek",
+                "Set",
+                "Static",
+                "Step",
+                "String",
+                "Then",
+                "Time",
+                "To",
+                "True",
+                "WithEvents",
+                "Dim",
+                "ReDim",
+                "Preserve",
+                "If",
+                "Then",
+                "Function",
+                "Sub",
+                "GoSub",
+                "GoTo",
+                "On",
+                "Error",
+                "Do",
+                "Until",
+                "End",
+                "Exit",
+                "While", 
+                "Loop", 
+                "And", 
+                "Or",
+                "\\+",
+                "\\&",
+                "\\=",
+                "\\-",
+                "\\*"
+            };
+
+
+            string[] funcKeywords = new string[]
+            {
+                "Mid",
+                "Mid$",
+                "Asc",
+                "Asc$",
+                "Chr",
+                "Chr$",
+                "UBound",
+                "LBound",
+                "Len",
+                "UCase",
+                "LCase"
+
+            };
+
+            StyleSheet styleSheet = new StyleSheet(Color.White);
+
+            string[] dangerKeywords = new string[]
+            {
+                            "CreateObject",
+                            "WScript.Shell",
+                            "WScript.GetObject",
+
+            };
+
+            foreach (var v in dangerKeywords.Distinct().ToArray())
+                styleSheet.AddStyle(v, Color.Red);
+
+            foreach (var v in vbKeywords.Distinct().ToArray())
+                styleSheet.AddStyle(v + "\\s+", Color.CornflowerBlue);
+
+            foreach (var v in funcKeywords.Distinct().ToArray())
+                styleSheet.AddStyle(v + "\\(", Color.LightBlue);
+
+
+
+            //foreach (var v in funcKeywords.Distinct().ToArray())
+            styleSheet.AddStyle("[a-zA-Z][\\w]*\\(", Color.SkyBlue);
+
+            styleSheet.AddStyle("\\)", Color.LightBlue);
+
+            styleSheet.AddStyle("\\\"(.*?)\\\"", Color.Orange);
+            styleSheet.AddStyle("\\\'(.*?).*", Color.Green);
+
+            Console.WriteLine(new string('═', 80));
+
+            Console.WriteLineStyled(result + "\r\n", styleSheet);
+
+            perfWatch.Stop();
+            Console.ForegroundColor = Color.WhiteSmoke;
+
+            Console.WriteLine(new string('═', 80));
+            Console.WriteLine($"# Printed in {perfWatch.ElapsedMilliseconds} ms.");
+
+        }
+
+        private static string DeobfuscateContent(string content, Options opts)
         {
             Stopwatch perfWatch = new Stopwatch();
 
@@ -60,13 +252,13 @@ namespace vbSparkle.CLI
 
             var result = VbPartialEvaluator.PrettifyEncoded(content, new EvaluatorOptions()
             {
-                SymbolRenamingMode = SymbolRenamingMode.None,
-                JunkCodeProcessingMode = JunkCodeProcessingMode.Remove,
-                IndentSpacing = 4,
+                SymbolRenamingMode = opts.SymbolRenamingMode,
+                JunkCodeProcessingMode = opts.JunkCodeProcessingMode,
+                IndentSpacing = opts.IndentSpacing
             });
 
             perfWatch.Stop();
-            Console.WriteLine($"Computed in {perfWatch.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"# Computed in {perfWatch.ElapsedMilliseconds} ms.");
 
             return result;
         }
